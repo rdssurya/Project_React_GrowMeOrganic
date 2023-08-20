99384fd0072cf7d29c3bb625eda6e9fe7228b5f3
rdssurya <rdssurya@gmail.com>
2023-08-20 15:27:30 +0530

    still working

--- my-node-app/my-react-app/src/App.tsx
+++ my-node-app/my-react-app/src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const userForm = document.getElementById('userForm') as HTMLFormElement;
-    userForm.addEventListener('submit', (e) => {
-        e.preventDefault();})
+  // const userForm = document.getElementById('userForm') as HTMLFormElement;
+  const [formData, setFormData] = useState({
+    name: '',
+    phone:'',
+    email: '',
+  });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handleSubmit=(event: React.FormEvent<HTMLFormElement>)=>{
+    event.preventDefault();
+    localStorage.setItem('userData', JSON.stringify(formData));
+  }
 
   return (
     <>
       <h1>User Information Form</h1>
-    <form id="userForm">
+    <form id="userForm" onSubmit={handleSubmit}>
         <div>
             <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" required></input>
+            <input type="text" id="name" name="name" value={formData.name}
+          onChange={handleChange} required></input>
         </div><br/>
         <div>
             <label htmlFor="phone">Phone Number:</label>
-            <input type="tel" id="phone" name="phone" pattern="[0-9]{10}" required></input>
+            <input type="tel" id="phone" name="phone" pattern="[0-9]{10}" value={formData.phone}
+          onChange={handleChange} required></input>
         </div>
         <br/>
         <div>
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required></input>
+            <input type="email" id="email" name="email" value={formData.email}
+          onChange={handleChange} required></input>
         </div>
         <br/>
         <div>
-            <button type="submit">Submit</button>
+            <button type="submit" >Submit</button>
         </div>
     </form>
     </>
   )
 }
 
-export default App
+export default App;
