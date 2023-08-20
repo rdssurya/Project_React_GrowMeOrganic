cf97559bdc5d3d2489df33ad5a49e4e36ca1ccf1
rdssurya <rdssurya@gmail.com>
2023-08-20 14:10:14 +0530

    working on the first page

--- my-node-app/my-react-app/src/App.tsx
+++ my-node-app/my-react-app/src/App.tsx
@@ -1,33 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const userForm = document.getElementById('userForm') as HTMLFormElement;
+    userForm.addEventListener('submit', (e) => {
+        e.preventDefault();})
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>User Information Form</h1>
+    <form id="userForm">
+        <div>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" required></input>
+        </div><br/>
+        <div>
+            <label htmlFor="phone">Phone Number:</label>
+            <input type="tel" id="phone" name="phone" pattern="[0-9]{10}" required></input>
+        </div>
+        <br/>
+        <div>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required></input>
+        </div>
+        <br/>
+        <div>
+            <button type="submit">Submit</button>
+        </div>
+    </form>
     </>
   )
 }
